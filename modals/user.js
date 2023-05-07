d4f3844dc7b79853d8d431d16eb5e694856e1fb4
Deepanshu Batra <80679706+Deepanshu979@users.noreply.github.com>
2023-05-07 13:12:47 +0530

    Updated Changes

--- modals/user.js
+++ modals/user.js
@@ -9,7 +9,6 @@ const userSchema = new mongoose.Schema({
   watchList: [
     {
       imdbId: "String",
-      name:"String"
     },
   ],
   googleId: String,
--- public/app.js
+++ public/app.js
@@ -285,5 +285,5 @@ function func() {
 
 function movieDetails(id) {
   window.location.href = "/" + id;
-  console.log(id);
+ 
 }
